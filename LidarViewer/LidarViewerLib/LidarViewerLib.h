e5a9718ac9cf51259da3b9ada06e79a3a5d53f13
restjohn <restjohn@users.noreply.github.com>
2016-07-05 13:12:00 -0600

    added a framework target for LidarViewerLib to include as a library instead of an app

---
+++ LidarViewer/LidarViewerLib/LidarViewerLib.h
@@ -0,0 +1,19 @@
+//
+//  LidarViewerLib.h
+//  LidarViewerLib
+//
+//  Created by Robert St. John on 7/5/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LidarViewerLib.
+FOUNDATION_EXPORT double LidarViewerLibVersionNumber;
+
+//! Project version string for LidarViewerLib.
+FOUNDATION_EXPORT const unsigned char LidarViewerLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LidarViewerLib/PublicHeader.h>
+
+
