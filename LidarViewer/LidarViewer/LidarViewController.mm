1fe8283f674455e2c3f0e25ea20ff4e456e2d1eb
restjohn <restjohn@users.noreply.github.com>
2016-07-05 08:17:24 -0600

    renamed ViewController class to LidarViewController

--- LidarViewer/LidarViewer/LidarViewController.mm
+++ LidarViewer/LidarViewer/LidarViewController.mm
@@ -6,18 +6,18 @@
 //  Copyright © 2015 mousebird consulting. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LidarViewController.h"
 #import "WhirlyGlobeComponent.h"
 //#import "AFHTTPRequestOperation.h"
 #import "LAZShader.h"
 #import "LAZReader.h"
 #import "LAZQuadReader.h"
 
-@interface ViewController () <WhirlyGlobeViewControllerDelegate>
+@interface LidarViewController () <WhirlyGlobeViewControllerDelegate>
 
 @end
 
-@implementation ViewController
+@implementation LidarViewController
 {
     WhirlyGlobeViewController *globeViewC;
     MaplyShader *pointShaderRamp,*pointShaderColor;
@@ -34,7 +34,7 @@
         if (![[NSFileManager defaultManager] fileExistsAtPath:dbPath])
             dbPath = nil;
     }
-    
+
     return dbPath;
 }
 
@@ -52,7 +52,7 @@
     [rampGen addHexColor:0xfdb600];
     [rampGen addHexColor:0xff6301];
     [rampGen addHexColor:0xf01a0a];
-    
+
     return [rampGen makeImage:CGSizeMake(256.0,1.0)];
 }
 
@@ -71,14 +71,14 @@ static int MaxDisplayedPoints = 3000000;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+
     // Overrides for various databases
     NSDictionary *dbDesc = @{@"ot_35121F2416_1-B-quad-data": @{kLAZReaderCoordSys:@"+proj=utm +zone=10 +datum=NAD83 +no_defs", kLAZShaderPointSize: @(4.0), kLAZReaderZOffset: @(2.0)},
                              @"st-helens-quad-data": @{kLAZReaderColorScale: @(255.0)},
                              @"stadium-utm-quad-data": @{kLAZReaderColorScale: @(255.0)}
                              };
 
-    
+
     // Set up the globe
     globeViewC = [[WhirlyGlobeViewController alloc] init];
     [self.view addSubview:globeViewC.view];
@@ -90,10 +90,10 @@ static int MaxDisplayedPoints = 3000000;
     globeViewC.tiltGesture = true;
     globeViewC.autoMoveToTap = false;
     globeViewC.twoFingerTapGesture = false;
-    
+
     // Give us a tilt
     [globeViewC setTiltMinHeight:0.001 maxHeight:0.01 minTilt:1.21771169 maxTilt:0.0];
-    
+
     // Add a base layer
     NSString * baseCacheDir = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     NSString * cacheDir = [NSString stringWithFormat:@"%@/maqquesttiles/", baseCacheDir];
@@ -106,11 +106,11 @@ static int MaxDisplayedPoints = 3000000;
     layer.drawPriority = 0;
     layer.color = [UIColor colorWithWhite:0.5 alpha:1.0];
     [globeViewC addLayer:layer];
-    
+
     // Shader Shaders for color and ramp versions
     pointShaderColor = BuildPointShader(globeViewC);
     pointShaderRamp = BuildRampPointShader(globeViewC,[self generateColorRamp]);
-    
+
     // Look for databases in the documents directory
     NSString *docDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
     for (NSString *path in [[NSFileManager defaultManager] subpathsAtPath:docDir])
@@ -119,7 +119,7 @@ static int MaxDisplayedPoints = 3000000;
         if ([ext isEqualToString:@"sqlite"])
         {
             NSString *base = [[path lastPathComponent] stringByDeletingPathExtension];
-            
+
             // Look for overrides
             NSDictionary *desc = dbDesc[base];
 
@@ -142,7 +142,7 @@ static int MaxDisplayedPoints = 3000000;
         else
             quadDelegate.shader = rampShader;
         [quadDelegate getBoundsLL:&ll ur:&ur];
-        
+
         // Start location
         WhirlyGlobeViewControllerAnimationState *viewState = [[WhirlyGlobeViewControllerAnimationState alloc] init];
         viewState.heading = -3.118891;
@@ -151,7 +151,7 @@ static int MaxDisplayedPoints = 3000000;
         MaplyCoordinate center = [[quadDelegate coordSys] localToGeo:[quadDelegate getCenter]];
         viewState.pos = MaplyCoordinateDMake(center.x,center.y);
         [globeViewC setViewState:viewState];
-        
+
         MaplyQuadPagingLayer *lazLayer = [[MaplyQuadPagingLayer alloc] initWithCoordSystem:quadDelegate.coordSys delegate:quadDelegate];
         // It takes no real time to fetch from the database.
         // All the threading is in projecting coordinates
@@ -162,7 +162,7 @@ static int MaxDisplayedPoints = 3000000;
         lazLayer.maxTileHeight = ur.z;
         lazLayer.useParentTileBounds = false;
         [globeViewC addLayer:lazLayer];
-        
+
         // Drop a label so the user can find it when zoomed out
         MaplyScreenLabel *label = [[MaplyScreenLabel alloc] init];
         label.text = [[dbPath lastPathComponent] stringByDeletingPathExtension];
