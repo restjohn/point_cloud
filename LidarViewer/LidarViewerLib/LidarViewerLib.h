8996081519e12cc4073cc8f60e8752447542f7fd
restjohn <restjohn@users.noreply.github.com>
2016-07-12 13:07:31 -0600

    added imports to module header

--- LidarViewer/LidarViewerLib/LidarViewerLib.h
+++ LidarViewer/LidarViewerLib/LidarViewerLib.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char LidarViewerLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LidarViewerLib/PublicHeader.h>
 
-
+#import <LidarViewerLib/LAZQuadReader.h>
+#import <LidarViewerLib/LAZShader.h>
+#import <LidarViewerLib/MeshBuilder.h>
